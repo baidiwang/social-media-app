2a3d11fd70bc14ee28644e89d54cfbb7a9476a12
Eric Blaney <EricBlaney@gmail.com>
2022-09-16 20:31:59 -0400

    Update index.js
    Added user store

--- client/store/index.js
+++ client/store/index.js
@@ -7,15 +7,18 @@ import likes from './like';
 import messages from './message';
 import posts from './post';
 import photos from './photo';
-;
-const reducer = combineReducers({ auth, comments, likes, messages, posts, photos })
+import users from './user';
+
+const reducer = combineReducers({ auth, comments, likes, messages, posts, photos, users })
 const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}));
 const store = createStore(reducer, middleware);
 
-export default store;
+
 export * from './auth';
 export * from './message';
 export * from './like';
 export * from './comment';
 export * from './photo';
 export * from './post';
+export * from './user';
+export default store;
