a32c3173083c27530c0f8eb08d2fc9482d72a933
Eric Blaney <EricBlaney@gmail.com>
2022-10-01 09:13:49 -0400

    deploy

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { connect } from "react-redux";
 import { authenticate } from "../store";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Box } from "@mui/material";
+import io from 'socket.io-client';
 
 export const Container = styled.div``;
 
